819993795c483e97daf586dc1173e6ef57fcb02f
Hans de Graaff <graaff@gentoo.org>
2019-12-20 09:57:10 +0100

    Remove unused base_settings variable
    Signed-off-by: Hans de Graaff <graaff@gentoo.org>

--- lib/kkuleomi/store.rb
+++ lib/kkuleomi/store.rb
@@ -9,27 +9,6 @@ module Kkuleomi::Store
 			CommitRepository
 		]
 
-    base_settings = {
-        analysis: {
-            filter: {
-                autocomplete_filter: {
-                    type: 'edge_ngram',
-                    min_gram: 1,
-                    max_gram: 20,
-                }
-            },
-            analyzer: {
-                autocomplete: {
-                    type: 'custom',
-                    tokenizer: 'standard',
-                    filter: %w(lowercase autocomplete_filter)
-                }
-            }
-        },
-				index: { mapper: { dynamic: false } },
-				mapping: { total_fields: { limit: 50000 } }
-    }
-
 		settings = JSON.parse('{ "mapping": { "total_fields": { "limit": 50000 } } }')
 
 		# In ES 1.5, we could use 1 mega-index. But in ES6, each model needs its own.
