1b24333de399e225d6f5930520b1df2dc5af1f44
Hans de Graaff <graaff@gentoo.org>
2019-12-20 10:40:59 +0100

    Fix markdown handler broken in Rails 6 migration
    Signed-off-by: Hans de Graaff <graaff@gentoo.org>

--- lib/core_ext/markdown_handler.rb
+++ lib/core_ext/markdown_handler.rb
@@ -5,7 +5,8 @@ module MarkdownHandler
     @erb ||= ActionView::Template.registered_template_handler(:erb)
   end
 
-  def self.call(_template, _source)
+  def self.call(template, source)
+    compiled_source = erb.call(template, source)
     "RDiscount.new(begin;#{compiled_source};end).to_html"
   end
 end
