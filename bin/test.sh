20b84a1c15a3ad0da9fed2232605a4a06cc39ed7
Max Magorsch <max@magorsch.de>
2020-01-01 03:58:23 +0100

    Fix the test failures
    So far the tests are failing when executed, using for example the
    docker-compose.test.yml file. To fix this, three things have changed:
    
      - the kkuleomi:update:all task has been added to bin/test.sh so that
        the test index is populated using the test data
    
      - kkuleomi_config.rb has been replaced by kkuleomi_config.rb.test
    
      - a repo/profiles/categories file has been created
    
    Signed-off-by: Max Magorsch <max@magorsch.de>

--- bin/test.sh
+++ bin/test.sh
@@ -7,5 +7,6 @@ bundler install
 bundle exec rake tmp:create RAILS_ENV=test
 bundle exec rake assets:precompile RAILS_ENV=test
 bundle exec rake kkuleomi:index:init RAILS_ENV=test
+bundle exec rake kkuleomi:update:all RAILS_ENV=test
 
 RAILS_ENV=test bundle exec rake test test/
